6143e58f68a094d417eabd5737397f70d2e024ed
Pratyaksh Bhardwaj <pratyakshb349@gmail.com>
2024-12-21 21:29:24 +0530

    made changes for login reg and wish

--- socfrontend/src/utils/api.js
+++ socfrontend/src/utils/api.js
@@ -3,26 +3,24 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/",
-  withCredentials: true,  // This is crucial for sending cookies
-  headers: {
-    'Content-Type': 'application/json',
-  }
+  // withCredentials: true,  // This is crucial for sending cookies
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // }
 });
 
 // Add request interceptor to include token from localStorage
-
-//Use for wishlist .. remove for register, checking what to do
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('authToken');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
