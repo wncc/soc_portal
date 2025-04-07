e7d3cbb021e175c7fc08f50abe41e002b062ff1d
Pratyaksh Bhardwaj <pratyakshb349@gmail.com>
2025-04-07 14:57:58 +0530

    edit project routes

--- socfrontend/src/URLGuard.js
+++ socfrontend/src/URLGuard.js
@@ -9,7 +9,7 @@ export default function URLGuard() {
   useEffect(() => {
     if (!role) return; // Avoid unnecessary redirections before role is loaded
 
-    const mentorRoutes = ['/mentor/home', '/mentor/add-project'];
+    const mentorRoutes = ['/mentor/home', '/mentor/add-project','/mentor/edit-project'];
     const menteeRoutes = ['/PreferenceFormFilled','/current_projects', '/wishlist', '/PreferenceForm'];
     // const authRoutes = ["/login", "/register", "/registerSuccess", "/verify-email"];
     const authRoutes = ['/login'];
