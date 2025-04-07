64fdface544485959924cf300cdd475d3aca7e8c
Pratyaksh2309 <163441330+Pratyaksh2309@users.noreply.github.com>
2025-04-07 17:36:54 +0530

    Update SSOButton.js

--- socfrontend/src/mentee/components/SSOButton.js
+++ socfrontend/src/mentee/components/SSOButton.js
@@ -40,7 +40,7 @@ const LoginButton = () => {
         className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 w-full"
         onClick={handleLoginRedirect}
       >
-          Login with IITB SSO
+          Login with ITC SSO
       </button>
       {/* <span className="text-sm text-gray-600 px-6 py-2 flex justify-center">
           No need to register if logging in with SSO
