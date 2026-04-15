aa88b77e56a9dc94d6b44129e483927471c8da0e
Pratyaksh Bhardwaj <pratyakshb349@gmail.com>
2026-04-15 20:18:53 +0530

    delete projects functionaity

--- socbackend/domains/migrations/0004_domain_project_deletion_open.py
+++ socbackend/domains/migrations/0004_domain_project_deletion_open.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('domains', '0002_auto_previous'),  # Update this to your latest migration
+        ('domains', '0003_add_project_permissions'),
     ]
 
     operations = [
